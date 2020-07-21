cf7b42d503d11f2db762dc775d235418c12a2b15
Rachel Collins <rachel.collins@my.liu.edu>
2020-07-21 15:51:27 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,7 @@
+def #display_rainbow(array_colors)
+
+
+display_rainbow = (['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+
+display_rainbow[0] << "R:"
 # Write your #display_rainbow method here
