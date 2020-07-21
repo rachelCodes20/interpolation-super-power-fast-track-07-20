6687eac51103f22961b7c4cb29836cb371f966c0
Rachel Collins <rachel.collins@my.liu.edu>
2020-07-21 16:01:24 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,7 +2,7 @@ def #display_rainbow(array_colors)
 rain_array: ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 def display_rainbow(colors) 
-  puts "R: #{}"
+  puts "R: #{}, O: #{}, Y: : #{}G: , B: #{}, I: #{}, V: #{}"
 
 display_rainbow[0] << "R:"
 # Write your #display_rainbow method here
