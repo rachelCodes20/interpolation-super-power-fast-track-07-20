fb6e2966d626094d42c7454343dd515728a2dcb5
Rachel Collins <rachel.collins@my.liu.edu>
2020-07-21 16:21:16 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -10,5 +10,6 @@ def display_rainbow(colors)
   puts "R: #{rain_array[0]}, O: #{rain_array[1]}, Y: #{rain_array[2]}, G: #{rain_array[3]}, B: #{rain_array[4]}, I: #{rain_array[5]}, V: #{rain_array[6]}"
   
  
+ end
  
- enddisplay_rainbow(colors)
+ display_rainbow(colors)
